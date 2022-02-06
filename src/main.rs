f2eff3eb487b78f79d3fad8e1a3793c6b6f32b47
Alexey Parfenov <alepar@gmail.com>
2022-02-06 14:54:56 -0800

    Always disconnects from peripheral

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,17 @@ use std::time::Duration;
 
 use btleplug::api::{ScanFilter};
 use btleplug::api::{Central, Manager as _, Peripheral};
-use btleplug::platform::Manager;
+use btleplug::platform::{Adapter, Manager};
 use bytes::{Buf, Bytes};
 use log::{debug, info, warn};
 use prometheus_hyper::{RegistryFn, Server};
 use tokio::sync::Notify;
-use prometheus::{Opts, Registry, Counter, TextEncoder, Encoder, IntCounter, GaugeVec, IntGaugeVec};
+use prometheus::{Opts, Registry, GaugeVec, IntGaugeVec};
 use prometheus::core::Collector;
 use tokio::time;
 use uuid::Uuid;
 
 extern crate pretty_env_logger;
-#[macro_use] extern crate log;
 
 const SENSORVALUES_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0xb42e2a68_ade7_11e4_89d3_123b93f75cba);
 const SENSORVALUES_SERVICE_UUID: Uuid = Uuid::from_u128(0xb42e1c08_ade7_11e4_89d3_123b93f75cba);
@@ -26,8 +25,111 @@ const SENSORVALUES_SERVICE_UUID: Uuid = Uuid::from_u128(0xb42e1c08_ade7_11e4_89d
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
-    let metrics = createMetrics();
+    let metrics = create_metrics();
+    let adapter_list = start_scanning().await
+        .expect("could not set adapters up to start scanning");
 
+    loop {
+        time::sleep(Duration::from_secs(5)).await;
+        query_peripherals(&metrics, &adapter_list).await;
+    }
+}
+
+async fn query_peripherals(metrics: &CustomMetrics, adapter_list: &Vec<Adapter>) {
+    for adapter in adapter_list.iter() {
+        let peripherals = adapter.peripherals().await;
+
+        if let Err(err) = peripherals {
+            warn!("Could not get peripherals: {:?}", err);
+            continue;
+        }
+
+        let peripherals = peripherals.unwrap();
+        if peripherals.is_empty() {
+            debug!("No peripheral devices found, skipping");
+            continue;
+        }
+
+        // All peripheral devices in range.
+        debug!("discovered {} peripherals", peripherals.len());
+        for peripheral in peripherals.iter() {
+            let properties = peripheral.properties().await;
+            if let Err(err) = properties {
+                debug!("Failed to read properties from peripheral, skipping: {:?}", err);
+                continue;
+            }
+
+            let properties = properties.unwrap();
+            if let None = properties {
+                continue;
+            }
+
+            let properties = properties.unwrap();
+            let manufacturer_data = properties.manufacturer_data;
+
+            if manufacturer_data.contains_key(&820) {
+                let md = manufacturer_data.get(&820).unwrap();
+                let serial =
+                    ((md[3] as u32) << 24) +
+                        ((md[2] as u32) << 16) +
+                        ((md[1] as u32) << 8) +
+                        (md[0] as u32);
+
+                // Connect if we aren't already connected.
+                if let Err(err) = peripheral.connect().await {
+                    debug!("Error connecting to peripheral, skipping: {:?}", err);
+                    continue;
+                }
+
+                query_peripheral(metrics, peripheral, serial).await;
+
+                if let Err(err) = peripheral.disconnect().await {
+                    debug!("failed to disconnect from peripheral {:X}: {:?}", properties.address, err);
+                }
+            }
+        }
+    }
+}
+
+async fn query_peripheral(metrics: &CustomMetrics, peripheral: &impl Peripheral, serial: u32) {
+    // discover services and characteristics
+    if let Err(err) = peripheral.discover_services().await {
+        debug!("Failed to discover services, skipping: {:?}", err);
+        return;
+    }
+
+    // find the characteristic we want
+    let chars = peripheral.characteristics();
+    let char = chars
+        .iter()
+        .find(|c| c.uuid == SENSORVALUES_CHARACTERISTIC_UUID);
+
+    if let None = char {
+        debug!("Failed to find correct characteristic, skipping");
+        return;
+    }
+    let char = char.unwrap();
+
+    let data = peripheral.read(char).await;
+    if let Err(err) = data {
+        debug!("Failed to read data from characteristic, skipping: {:?}", err);
+        return;
+    }
+
+    let values = SensorValues::from_vec(data.unwrap());
+    info!("serial {}, {:?}", serial, values);
+
+    let label_values = &[&*serial.to_string()];
+    metrics.gauge_humidity.with_label_values(label_values).set(values.humidity as f64);
+    metrics.gauge_temp.with_label_values(label_values).set(values.temp as f64);
+    metrics.gauge_atm.with_label_values(label_values).set(values.atm as f64);
+    metrics.gauge_radon_short.with_label_values(label_values).set(values.radon_short as i64);
+    metrics.gauge_radon_long.with_label_values(label_values).set(values.radon_long as i64);
+    metrics.gauge_co2.with_label_values(label_values).set(values.co2 as i64);
+    metrics.gauge_voc.with_label_values(label_values).set(values.voc as i64);
+}
+
+async fn start_scanning() -> Result<Vec<Adapter>, Box<dyn Error>> {
     let manager = Manager::new().await?;
     let adapter_list = manager.adapters().await?;
     if adapter_list.is_empty() {
@@ -37,107 +139,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     for adapter in adapter_list.iter() {
         info!("Starting scan...");
         adapter
-            .start_scan(ScanFilter{services: vec!(SENSORVALUES_SERVICE_UUID)})
+            .start_scan(ScanFilter { services: vec!(SENSORVALUES_SERVICE_UUID) })
             .await
             .expect("Can't scan BLE adapter for connected devices...");
     }
 
-    loop {
-        time::sleep(Duration::from_secs(5)).await;
-
-        for adapter in adapter_list.iter() {
-            let peripherals = adapter.peripherals().await;
-
-            if let Err(err) = peripherals {
-                warn!("Could not get peripherals: {:?}", err);
-                continue;
-            }
-
-            let peripherals = peripherals.unwrap();
-
-            if peripherals.is_empty() {
-                debug!("No peripheral devices found, skipping");
-            } else {
-                // All peripheral devices in range.
-                debug!("discovered {} peripherals", peripherals.len());
-                for peripheral in peripherals.iter() {
-                    let properties = peripheral.properties().await;
-                    if let Err(err) = properties {
-                        debug!("Failed to read properties from peripheral, skipping: {:?}", err);
-                        continue;
-                    }
-
-                    let properties = properties.unwrap();
-                    if let None = properties {
-                        continue;
-                    }
-
-                    let properties = properties.unwrap();
-                    let manufacturer_data = properties.manufacturer_data;
-
-                    if manufacturer_data.contains_key(&820) {
-                        let md = manufacturer_data.get(&820).unwrap();
-                        let serial =
-                            ((md[3] as u32) << 24) +
-                            ((md[2] as u32) << 16) +
-                            ((md[1] as u32) << 8) +
-                            (md[0] as u32);
-
-                        // Connect if we aren't already connected.
-                        if let Err(err) = peripheral.connect().await {
-                            debug!("Error connecting to peripheral, skipping: {:?}", err);
-                            continue;
-                        }
-
-                        // discover services and characteristics
-                        if let Err(err) = peripheral.discover_services().await {
-                            debug!("Failed to discover services, skipping: {:?}", err);
-                            continue;
-                        }
-
-                        // find the characteristic we want
-                        let chars = peripheral.characteristics();
-                        let char = chars
-                            .iter()
-                            .find(|c| c.uuid == SENSORVALUES_CHARACTERISTIC_UUID);
-
-                        if let None = char {
-                            debug!("Failed to find correct characteristic, skipping");
-                            continue;
-                        }
-                        let char = char.unwrap();
-
-                        let data = peripheral.read(char).await;
-                        if let Err(err) = data {
-                            debug!("Failed to read data from characteristic, skipping: {:?}", err);
-                            continue;
-                        }
-
-                        let values = SensorValues::from_vec(data.unwrap());
-                        info!("serial {}, {:?}", serial, values);
-
-                        let label_values = &[&*serial.to_string()];
-                        metrics.gauge_humidity.with_label_values(label_values).set(values.humidity as f64);
-                        metrics.gauge_temp.with_label_values(label_values).set(values.temp as f64);
-                        metrics.gauge_atm.with_label_values(label_values).set(values.atm as f64);
-                        metrics.gauge_radon_short.with_label_values(label_values).set(values.radon_short as i64);
-                        metrics.gauge_radon_long.with_label_values(label_values).set(values.radon_long as i64);
-                        metrics.gauge_co2.with_label_values(label_values).set(values.co2 as i64);
-                        metrics.gauge_voc.with_label_values(label_values).set(values.voc as i64);
-
-                        if let Err(err) = peripheral.disconnect().await {
-                            debug!("failed to disconnect from peripheral {:X}: {:?}", properties.address, err);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    Ok(())
+    Ok(adapter_list)
 }
 
-fn createMetrics() -> CustomMetrics {
+fn create_metrics() -> CustomMetrics {
     let registry = Arc::new(Registry::new());
     let shutdown = Arc::new(Notify::new());
     let shutdown_clone = Arc::clone(&shutdown);
